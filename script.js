e2f597d8952aff65f0bb98e9a4cdb8afae614ea7
Mads Vind Knudsen <73589644+MadsVind@users.noreply.github.com>
2023-03-03 10:02:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hi");
+
+
+let blobArray = [{type: "img",
+                  href: "https://github.com/MadsVind", 
+                  title: "Github link to MadsVind", 
+                  content: imgBlobOutput("https://avatars.githubusercontent.com/u/73589644?s=40&amp;v=4", "@MadsVind")},
+                 
+                 {type: "txt",
+                  href: "email-form.html",             
+                  title: "Contact me",              
+                  content: "@"}];
+
+function addBlop() {
+  main = document.querySelector(body>main);
+}
+
+function imgBlobOutput(src, alt) {
+  let imgHTML = `<img src=${src}alt=${alt} data-view-component=\"true\" class=\"imgBlob\"> </a>`
+  
+  retun imgHTML;
+}
